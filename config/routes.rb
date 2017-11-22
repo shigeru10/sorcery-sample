18df381c7bb3d4479fe122966d0a1e7cb5f35f7e
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-11-22 19:19:55 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: 'users#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login', to: 'user_sessions#create', as: :login
+  post 'logout', to: 'user_sessions#destroy', as: :logout
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
